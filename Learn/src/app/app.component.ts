3b9683b4472881cd24b7c25d76e5c6410c2065f7
fadyehabamer <fadyamer45@gmail.com>
2025-01-12 23:26:13 +0200

    clean start

--- Learn/src/app/app.component.ts
+++ Learn/src/app/app.component.ts
@@ -1,35 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NewComponentComponent } from './new-component/new-component.component';
-import { CourseCardComponent } from "./course-card/course-card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NewComponentComponent, CourseCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
   title = 'Learn';
   Interpolation = 'Interpolation';
   data = {
     name: 'Angular',
-    version: 9
-  }
-
-
-  readonly Courses:any = [
-    { title: 'Angular', author: 'Mosh' },
-    { title: 'React', author: 'Mosh' },
-    { title: 'Vue', author: 'Mosh' }
-  ]
-
-
-  bestCourse = this.Courses[0];
-  secondBestCourse = this.Courses[1];
-  thirdBestCourse = this.Courses[2];
-
-
-
+    version: 9,
+  };
 }
