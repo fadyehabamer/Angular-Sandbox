037491ef44c4a55590e695017aa1e69b98a1b121
fadyehabamer <fadyamer45@gmail.com>
2025-01-20 21:35:01 +0200

    clean project

--- Learn/src/app/app.component.ts
+++ Learn/src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductsService } from './services/products.service';
-import { Products } from '../types';
+
 
 import { PrimeNG } from 'primeng/config';
-
-import { HeaderComponent } from "./layout/header/header.component";
-import { FooterComponent } from "./layout/footer/footer.component";
-import { ProductComponent } from "./components/product/product.component";
-
 import { Product } from '../types';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ProductComponent , CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -23,7 +16,7 @@ export class AppComponent {
 
   // constructor
   // before the component is created
-  constructor(private productService: ProductsService , private primeng: PrimeNG) {
+  constructor(private primeng: PrimeNG) {
   }
 
 
@@ -33,20 +26,9 @@ export class AppComponent {
     console.log(product);
   }
 
-  // ngOnInit
-  // after the component is created
+  
   ngOnInit() {
 
     this.primeng.ripple.set(true);
-
-    this.productService
-      .getInternalProducts('http://localhost:3000/clothes', {
-        page: 0,
-        perPage: 5,
-      })
-      .subscribe((products: Products) => {
-        // console.log(products.items);
-        this.products = products.items;
-      });
   }
 }
